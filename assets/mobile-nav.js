5fa8473a12c295b02356e1fa5c6321310cd95386
Hao Tran <account@haothitran.com>
2020-08-10 16:38:03 -0400

    Update mobile navigation - hidden animated nav

---
+++ assets/mobile-nav.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("site-nav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("site-nav").style.width = "0";
+}
